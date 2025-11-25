9cb9baa197079e5bc9aad42db9dd983925df9923
Nagaratna Hegde <nagaratnahegde@Nagaratnas-MacBook-Air.local>
2025-11-25 16:20:58 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+app.use("/test", (req, res) => {
+  res.send("Hello World");
+});
+app.listen(3000, () => console.log("Listening on port 3000"));
