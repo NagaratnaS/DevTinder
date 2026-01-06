7b5dbbd5ceca85018df0557bb837072ddfd54519
Nagaratna Hegde <nagaratnahegde@Nagaratnas-MacBook-Air.local>
2026-01-06 22:01:21 +0530

    added search methods

--- src/app.js
+++ src/app.js
@@ -5,6 +5,37 @@ const User = require("./models/user");
 
 app.use(express.json());
 
+app.get("/getUser", async (req, res) => {
+  try {
+    const users = await User.find({ emailId: req.body.emailId });
+    if (users.length === 0) {
+      res.status(404).send("No users found with the given emailId");
+    } else {
+      res.send(users);
+    }
+  } catch (error) {
+    res.status(400).send(`Error Fetching Data${error.message}`);
+  }
+});
+
+app.get("/getOneUser", async (req, res) => {
+  try {
+    const user = await User.findOne({ emailId: req.body.emailId });
+    res.send(user);
+  } catch (error) {
+    res.status(400).send(`Error Fetching Data${error.message}`);
+  }
+});
+
+app.get("/findAllUsers", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(400).send(`Error Fetching Data${error.message}`);
+  }
+});
+
 app.post("/signup", async (req, res) => {
   const user = new User(req.body);
   try {
