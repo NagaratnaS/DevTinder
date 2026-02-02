339f238399fefdbe2d9c2bf3ceb8a74b062af66e
Nagaratna Hegde <nagaratnahegde@Nagaratnas-MacBook-Air.local>
2026-02-02 11:53:43 +0530

    get all connection requests api and populate

--- src/app.js
+++ src/app.js
@@ -11,10 +11,12 @@ app.use(cookieParser());
 const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/requests");
+const userRouter = require("./routes/user");
 
-app.use("/auth", authRouter);
-app.use("/user", profileRouter);
-app.use("/requests", requestRouter);
+app.use("/", authRouter);
+app.use("/", profileRouter);
+app.use("/", requestRouter);
+app.use("/", userRouter);
 
 // app.get("/findOneUSer", async (req, res) => {
 //   try {
---
+++ src/routes/user.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const { userAuth } = require("../middlewares/auth");
+const userRouter = express.Router();
+const ConnectionRequest = require("../models/connectionRequest");
+
+const USER_SAFE_DATA = "firstName lastName";
+
+userRouter.get("/user/requests/received", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const connectionRequest = await ConnectionRequest.find({
+      toUserId: loggedInUser._id,
+      status: "Interested",
+    }).populate("fromUserId", ["firstName", "lastName"]);
+    //populate("fromUserId", "firstName" "lastName");
+    res.json({
+      message: "Connection requests fetched successfully",
+      data: connectionRequest,
+    });
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
+  }
+});
+
+userRouter.get("", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [
+        { toUserId: loggedInUser._id, status: "Accepted" },
+        { fromUserId: loggedInUser._id, status: "Accepted" },
+      ],
+    }).populate("fromUserId", USER_SAFE_DATA);
+
+    const data = connectionRequests.map((row) => row.fromUserId);
+
+    res.json({ data: data });
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
+  }
+});
+
+module.exports = userRouter;
