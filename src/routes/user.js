be9a6b45b2fea2cd1377f0c6d97c37ab54a5fd86
Nagaratna Hegde <nagaratnahegde@Nagaratnas-MacBook-Air.local>
2026-02-03 19:02:12 +0530

    implemented pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -51,6 +51,9 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
 userRouter.get("/user/feed", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
     //find all connection request sent or received by logged in user
     const connectionRequests = await ConnectionRequest.find({
       $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
@@ -65,7 +68,10 @@ userRouter.get("/user/feed", userAuth, async (req, res) => {
         { _id: { $nin: Array.from(hideUsersFromFeed) } },
         { _id: { $ne: loggedInUser._id } },
       ],
-    }).select(USER_SAFE_DATA);
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
     res.send({ data: users });
   } catch (err) {
     res.status(400).send("ERROR:" + err.message);
